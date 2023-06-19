48484b40ee0d68b0bbee38dfce964e07068c544f
Ben Cochrane <ben.cochrane2112@gmail.com>
2023-06-19 13:32:02 -0400

    Make it work

---
+++ Assets/Status92/KCC/S92Controller2D.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using UnityEngine;
+
+namespace Status92.KCC
+{
+    [RequireComponent(typeof(S92ControllerMotor))]
+    public class S92Controller2D : MonoBehaviour
+    {
+        public S92ControllerMotor Motor;
+
+        public float XSpeed = 5;
+        public float LaunchSpeed = 5f;
+        public Vector2 ShortHopForce = Vector2.down;
+
+        public float MaxSpeed = 10;
+
+        public float JumpStrength = 15;
+        
+        public AnimationCurve RunSpeedCurve = AnimationCurve.Linear(0, 0, 1, 1);
+
+        public float RunSpeedTime;
+        public float TurnAroundTime;
+        public int LastRunSpeedSign;
+
+        private void Awake()
+        {
+            Motor = GetComponent<S92ControllerMotor>();
+        }
+
+        public void UpdateVelocity(ref Vector2 velocity)
+        {
+            var inputX = Input.GetAxis("Horizontal");
+            var inputScaled = Time.deltaTime * inputX * XSpeed;
+            velocity.x += inputScaled;
+            var inputSignX = Math.Sign(inputX);
+            var velocitySignX = Math.Sign(velocity.x);
+
+            if (inputSignX == LastRunSpeedSign)
+            {
+                RunSpeedTime += Time.deltaTime;
+                TurnAroundTime = 0;
+            }
+            else
+            {
+                RunSpeedTime = 0;
+            }
+            LastRunSpeedSign = inputSignX;
+
+            if (velocitySignX != inputSignX)
+            {
+                TurnAroundTime += Time.deltaTime;
+            }
+            else
+            {
+                TurnAroundTime = 0;
+            }
+
+            if (TurnAroundTime > 0.1f)
+            {
+                velocity.x *= -1f;
+            }
+
+            if (RunSpeedTime is < 0.25f and > 0f)
+            {
+                velocity.x += LaunchSpeed * Time.deltaTime;
+            }
+
+            var absVelocityX = Mathf.Abs(velocity.x);
+            var absInputX = Mathf.Abs(inputX);
+
+            if (absVelocityX > MaxSpeed)
+            {
+                velocity.x = Mathf.Sign(velocity.x) * MaxSpeed;
+            }
+
+            if (Math.Abs(inputSignX - velocitySignX) > 0.1f)
+            {
+                velocity.x += inputScaled * 5f;
+            }
+
+            if (absInputX < 0.01f || !Mathf.Approximately(inputSignX, velocitySignX))
+            {
+                velocity.x *= Mathf.Clamp01(1 - Time.deltaTime * 7);
+            }
+
+            if (Input.GetButtonDown("Jump") && Motor.Grounded)
+            {
+                velocity.y = JumpStrength;
+                Motor.Jump();
+            }
+
+            if (!Input.GetButton("Jump") && !Motor.Grounded && velocity.y > 0f)
+            {
+                velocity += ShortHopForce * Time.deltaTime;
+            }
+        }
+    }
+}
--- Assets/Status92/KCC/S92ControllerMotor.cs
+++ Assets/Status92/KCC/S92ControllerMotor.cs
@@ -2,13 +2,131 @@
 
 namespace Status92.KCC
 {
-    
     [RequireComponent(typeof(Rigidbody2D))]
-    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(Collider2D))]
     public class S92ControllerMotor : MonoBehaviour
     {
+        public S92Controller2D Controller;
+
+        private Collider2D Collider;
+        private Rigidbody2D Rigidbody;
+
+        public Vector2 Velocity;
+        public Vector2 Gravity = Vector2.one;
+        public Vector2 _surfaceAnchor = new Vector2(0, -0.1f);
+
+        public Rigidbody2D.SlideMovement SlideMovement;
+
+        private void Awake()
+        {
+            Collider = GetComponent<Collider2D>();
+            Rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            CalculateVelocity();
+            Move();
+        }
+
+        public bool Grounded;
+        public bool _skipGrounding;
+
+        private RaycastHit2D[] _raycastHits = new RaycastHit2D[1];
         
-        
-        
+        public ContactFilter2D _contactFilter = new();
+
+        public float _jumpTimer = 0f;
+
+        private void CalculateVelocity()
+        {
+            Controller.UpdateVelocity(ref Velocity);
+            
+            SlideMovement.surfaceAnchor = _skipGrounding ? Vector2.zero : _surfaceAnchor;
+            
+            var hitCount = Rigidbody.Cast(Vector2.down, _contactFilter, _raycastHits, 0.1f);
+            
+            if (hitCount > 0)
+            {
+                Grounded = true;
+            }
+            else
+            {
+                Grounded = false;
+            }
+
+            if (!Grounded)
+            {
+                Velocity += Gravity * Time.deltaTime;                
+            }
+
+            if (_jumpTimer > 0f)
+            {
+                _jumpTimer -= Time.deltaTime;
+                if (_jumpTimer <= 0f)
+                {
+                    _skipGrounding = false;
+                    _jumpTimer = -0.1f;
+                }
+            }
+        }
+
+        public void Jump()
+        {
+            if (_skipGrounding) return;
+            _surfaceAnchor = SlideMovement.surfaceAnchor;
+            Grounded = false;
+            _skipGrounding = true;
+            _jumpTimer = 0.1f;
+        }
+
+        private void Move()
+        {
+            // SlideMovement.gravity = Gravity;
+
+            var results = Rigidbody.Slide(Velocity, Time.deltaTime, SlideMovement);
+
+            if (!results.slideHit && !results.surfaceHit)
+            {
+                return;
+            }
+
+            if (Mathf.Abs(results.surfaceHit.normal.y) > 0.5f || Mathf.Abs(results.slideHit.normal.y) > 0.5f)
+            {
+                // Debug.Log($"Hit a vertical surface! {results.surfaceHit.normal}");
+                Velocity.y = 0f;
+            }
+
+            if (Mathf.Abs(results.surfaceHit.normal.x) > 0.75f && Vector2.Dot(Velocity, results.surfaceHit.normal) < -0.75)
+            {
+                Debug.Log($"Hit a horizontal surface! {results.surfaceHit.normal} {results.iterationsUsed} {results.remainingVelocity}");
+                Velocity.x = 0f;
+            }
+            
+            Debug.Log($"remaining velocity {results.remainingVelocity}");
+            
+            if (results.slideHit.rigidbody == null) return;
+            
+            Debug.Log($"Hit a rigidbody! {results.slideHit.rigidbody.name} {results.slideHit.normal}");
+            
+            var rb = results.slideHit.rigidbody;
+            var point = results.slideHit.point;
+
+            var distanceFromFeet = Vector2.Distance(point,
+                transform.position + new Vector3(0, Collider.bounds.size.y / 2f));
+
+            // if (!(distanceFromFeet > 1f))
+            {
+                // Debug.Log($"Only applying force if distance from feet is greater than 1f {distanceFromFeet}");
+                // return;
+            }
+            
+            Debug.Log($"Adding force at {point}, {transform.position} distance from feet {distanceFromFeet}");
+            rb.AddForceAtPosition(
+                Velocity * 0.1f, 
+                point,
+                ForceMode2D.Impulse
+            );
+        }
     }
 }
